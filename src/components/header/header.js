72cb39ed406957a964ce2ef296d1a9b423dee0a4
Eben Burger <eben@arlowconsulting.co.za>
2022-01-27 22:39:22 +0200

    header state and custom right menu added

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,29 +1,43 @@
-import React from "react";
+import React, {useState} from "react";
 import logo from '../images/logo.png'
 import styled from 'styled-components'
-import SearchBar from "../searchBar/searchBar";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {faStar} from "@fortawesome/free-regular-svg-icons";
+import {faAngleDoubleRight, faTimes} from "@fortawesome/free-solid-svg-icons";
+import ToggleViewDisplay from "../toggleViewDisplay/toggleViewDisplay";
 
-function Header () {
+const Header = () => {
+
+    const [isOpen, setIsOpen] = useState(false)
+
     return (
         <>
             <HeaderContainer>
                 <ImageWrapper>
                     <img src={logo} alt='IMDB Logo'/>
-                    <FontAwesomeIcon icon={faStar} />
                 </ImageWrapper>
-                <SearchBar />
+                <RightMenu>
+                    <IconContainer onClick={() => setIsOpen(!isOpen)}>
+                        <FontAwesomeIcon on className='icon' icon={faStar} />
+                        <p>Favorites</p>
+                    </IconContainer>
+                </RightMenu>
+                <Favorite show={isOpen}>
+                    <CustomClose onClick={() => setIsOpen(!isOpen)}>
+                        <FontAwesomeIcon icon={faTimes} />
+                    </CustomClose>
+                    <p>Favorite</p>
+                </Favorite>
             </HeaderContainer>
         </>
     )
 }
 
 const HeaderContainer = styled.div`
-  //position: relative;
-  //overflow-x: hidden;
-  display: block;
+  display: flex;
   background-color: red;
+  justify-content: space-evenly;
+  width: 100%;
 `;
 
 const ImageWrapper = styled.div`
@@ -34,6 +48,87 @@ const ImageWrapper = styled.div`
     width: 10rem;
     height: auto;
   }
+`;
+
+const RightMenu = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const IconContainer = styled.div`
+  font-size: 3rem;
+  cursor: pointer;
+  
+  
+  .icon{
+    display: flex;
+    align-content: center;
+    height: 100%;
+    margin-right: 2rem;
+    transition: transform 2s ease-in-out;
+  }
+  
+  p {
+    font-size: 1rem;
+    position: absolute;
+    right: 1rem;
+    display: none;
+  }
+  :hover p {
+    display: block;
+  }
+  :hover .icon {
+    animation: rotate-icon 2s linear 1;
+  }
+
+  @keyframes rotate-icon {
+    0% {
+      transform: rotate(0);
+    }
+    100% {
+      transform: rotate(-360deg);
+    }
+`;
+
+const Favorite = styled.div`
+    position: fixed;
+    top: 6.7rem;
+    bottom: 0;
+    right: 0;
+    background: slategrey;
+    width: 250px;
+    z-index: 2;
+    list-style: none;
+    padding: 20px;
+    display: flex;
+    flex-direction: column;
+    text-align: start;
+    transform: ${props => props.show ? 'translateY(0)' : 'translateY(100%)'};
+    transition: transform 0.5s ease-in-out;
+}
+    .fa-times {
+      font-size: 1.5rem;
+      width: 1rem;
+      height: 1rem;
+      position: absolute;
+      transition: transform 2s ease-in-out;
+    }
+    
+    :hover .fa-times {
+      animation: rotate-icon 2s linear 1;
+    }
+    @media (max-width: 420px) {
+      width: 90vw;
+    }
+`;
+
+const CustomClose = styled.div`
+    display: flex;
+    justify-content: flex-end;
+    
+    .fa-times {
+        cursor: pointer;
+    }
 `;
 
 
